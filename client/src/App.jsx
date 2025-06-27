33dfebaf0eab811096e0c20d00757ceaeff10c5d
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-06-28 03:30:49 +0530

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import {ScrollTrigger , SplitText} from "gsap/all"
 import gsap from "gsap/gsap-core"
+import Navbar from "./components/Navbar"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="">aaa</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { navLinks } from '../../constants'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+
+export default function Navbar() {
+
+    useGSAP(()=>{
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: "nav",
+                start: "bottom top",
+                scrub: true
+            }
+        })
+
+        navTween.fromTo(
+                "nav",
+                {backgroundColor: "transparent"},
+                {backgroundColor: '#00000050',
+                backgroundFilter: "blur(10px)",
+                duration: 1,
+                ease: "power1.inOut"
+                }
+
+            )
+    })
+
+    return (
+        <nav>
+            <div>
+                <a href="#home" className='flex items-center gap-2'>
+                    <p className='text-3xl text-transparent bg-clip-text bg-gradient-to-r from-amber-500 to-pink-600 font-bold'>Elixir</p>
+                </a>
+
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href="#{link.id}">{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+        
+    )
+}
